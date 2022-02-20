e0e6fd5cfec68273f3c3759684d31d39f9af4482
Sebastian <sebskogen@gmail.com>
2022-02-20 13:17:59 +0100

    3b: think its working

--- src/main.cpp
+++ src/main.cpp
@@ -68,8 +68,8 @@ int main(int argc, const char* argb[])
 {
     arrrgh::parser parser("glowbox", "Small breakout like juggling game");
     const auto& showHelp       = parser.add<bool>("help", "Show this help message.", 'h', arrrgh::Optional, false);
-    const auto& enableMusic    = parser.add<bool>("enable-music", "Play background music while the game is playing", 'm', arrrgh::Optional, false);
-    const auto& enableAutoplay = parser.add<bool>("autoplay", "Let the game play itself automatically. Useful for testing.", 'a', arrrgh::Optional, false);
+    const auto& enableMusic    = parser.add<bool>("enable-music", "Play background music while the game is playing", 'm', arrrgh::Optional, true);
+    const auto& enableAutoplay = parser.add<bool>("autoplay", "Let the game play itself automatically. Useful for testing.", 'a', arrrgh::Optional, true);
 
     // If you want to add more program arguments, define them here,
     // but do not request their value here (they have not been parsed yet at this point).
